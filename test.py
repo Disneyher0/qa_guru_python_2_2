6a633279ee33784d7bf05dd9fda69a210000a1d1
Disneyher0 <112819997+Disneyher0@users.noreply.github.com>
2022-09-05 18:59:47 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from selene.support.shared import browser
-from selene import be, have
-import pytest
-
-@pytest.fixture(scope="sessions", autouse=True)
-def window_size():
-    browser.config.window_height = 500
-    browser.config.window_width = 1000
