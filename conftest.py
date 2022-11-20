d50143fad86561565a42d3aa9b999da4fc9dc3f1
darkwing_duck <darkwing_duck@internet.ru>
2022-11-20 23:15:04 +0300

    Добавил сообщение '2.0'  в READY.md

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+@pytest.fixture(scope='session' , autouse=True)
+def open_browser():
+    print('Я вызываюсь перед тестом!')
+    yield
+    print('Я фикстура вызываюсь после теста')
